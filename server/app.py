4e17b6f574ac6e30043a70ff7b75c162526f349a
cyndy <cindyv.2436@gmail.com>
2026-02-11 23:30:19 -0500

    Updated user

--- server/app.py
+++ server/app.py
@@ -17,7 +17,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(255), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
 
     def to_dict(self):
         return {
